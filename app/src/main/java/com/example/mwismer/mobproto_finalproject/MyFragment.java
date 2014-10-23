5eb1a19aa1bf55568d13b4f72c8b6fc7812526f3
MattWis <matthew.wismer@students.olin.edu>
2014-10-23 19:20:39 -0400

    Move MyFragment into it's own class

---
+++ app/src/main/java/com/example/mwismer/mobproto_finalproject/MyFragment.java
@@ -0,0 +1,22 @@
+package com.example.mwismer.mobproto_finalproject;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by mwismer on 10/23/14.
+ */
+public class MyFragment extends Fragment{
+    public MyFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_my, container, false);
+        return rootView;
+    }
+}
