be65022a001ee1a864d2e90ae7b4d1c1edaad9fc
MattWis <matthew.wismer@students.olin.edu>
2014-10-23 19:24:22 -0400

    Enable Bluetooth if not already enabled

--- app/src/main/java/com/example/mwismer/mobproto_finalproject/MyFragment.java
+++ app/src/main/java/com/example/mwismer/mobproto_finalproject/MyFragment.java
@@ -1,7 +1,12 @@
 package com.example.mwismer.mobproto_finalproject;
 
 import android.app.Fragment;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +15,9 @@ import android.view.ViewGroup;
  * Created by mwismer on 10/23/14.
  */
 public class MyFragment extends Fragment{
+    private String TAG = "MyFragment";
+    private BluetoothAdapter mBLEAdapter;
+
     public MyFragment() {
     }
 
@@ -17,6 +25,15 @@ public class MyFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_my, container, false);
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getActivity().getSystemService(Context.BLUETOOTH_SERVICE);
+        mBLEAdapter = bluetoothManager.getAdapter();
+        if (mBLEAdapter == null || !mBLEAdapter.isEnabled()) {
+            Log.d(TAG, "Enabling Bluetooth");
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            getActivity().startActivityForResult(enableBtIntent, 20);
+        }
+
         return rootView;
     }
 }
