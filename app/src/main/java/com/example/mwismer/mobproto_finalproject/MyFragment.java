11be994fc90c4921df716e7b1eedb326b3fb3904
MattWis <matthew.wismer@students.olin.edu>
2014-10-23 19:31:08 -0400

    Skeleton for BLE scan

--- app/src/main/java/com/example/mwismer/mobproto_finalproject/MyFragment.java
+++ app/src/main/java/com/example/mwismer/mobproto_finalproject/MyFragment.java
@@ -2,6 +2,7 @@ package com.example.mwismer.mobproto_finalproject;
 
 import android.app.Fragment;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
@@ -10,6 +11,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by mwismer on 10/23/14.
@@ -34,6 +38,33 @@ public class MyFragment extends Fragment{
             getActivity().startActivityForResult(enableBtIntent, 20);
         }
 
+        scanBLE();
+
         return rootView;
+    }
+
+    private boolean mScanning = false;
+    private BluetoothAdapter.LeScanCallback mBLECallback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(BluetoothDevice bluetoothDevice, int i, byte[] bytes) {
+        }
+    };
+
+    private void scanBLE() {
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                mScanning = false;
+                mBLEAdapter.stopLeScan(mBLECallback);
+                Log.d(TAG, "BLE Scan finished");
+            }
+        };
+        long SCAN_PERIOD = 10000; //Time to scan in ms
+        timer.schedule(task, SCAN_PERIOD);
+
+        mScanning = true;
+        mBLEAdapter.startLeScan(mBLECallback);
+        Log.d(TAG, "BLE Scan started");
     }
 }
