91d6553affab5a06cd156b5e4484dab2fc102f77
MattWis <matthew.wismer@students.olin.edu>
2014-10-29 23:32:32 -0400

    Discover BLE services

--- app/src/main/java/com/example/mwismer/mobproto_finalproject/MyFragment.java
+++ app/src/main/java/com/example/mwismer/mobproto_finalproject/MyFragment.java
@@ -3,9 +3,16 @@ package com.example.mwismer.mobproto_finalproject;
 import android.app.Fragment;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -19,6 +26,7 @@ import java.util.TimerTask;
  * Created by mwismer on 10/23/14.
  */
 public class MyFragment extends Fragment{
+    private BluetoothDevice device = null;
     private String TAG = "MyFragment";
     private BluetoothAdapter mBLEAdapter;
 
@@ -47,6 +55,10 @@ public class MyFragment extends Fragment{
     private BluetoothAdapter.LeScanCallback mBLECallback = new BluetoothAdapter.LeScanCallback() {
         @Override
         public void onLeScan(BluetoothDevice bluetoothDevice, int i, byte[] bytes) {
+            if (device == null || !bluetoothDevice.equals(device)) {
+                Log.d(TAG, "Resetting device");
+                device = bluetoothDevice;
+            }
         }
     };
 
@@ -58,13 +70,53 @@ public class MyFragment extends Fragment{
                 mScanning = false;
                 mBLEAdapter.stopLeScan(mBLECallback);
                 Log.d(TAG, "BLE Scan finished");
+                if (device != null) {
+                    device.connectGatt(getActivity(), false, new BluetoothGattCallback() {
+                        @Override
+                        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+                            super.onConnectionStateChange(gatt, status, newState);
+                            String intentAction;
+                            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                                Log.i(TAG, "Connected to GATT server.");
+                                Log.i(TAG, "Attempting to start service discovery:" + gatt.discoverServices());
+
+                            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                                Log.i(TAG, "Disconnected from GATT server.");
+                            }
+                        }
+
+                        @Override
+                        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+                            super.onServicesDiscovered(gatt, status);
+                            for (BluetoothGattService service: gatt.getServices()) {
+                                Log.d(TAG, service.toString() + "::" + service.getCharacteristics().toString());
+                            }
+                        }
+
+                        //TODO: Use these two functions
+                        @Override
+                        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+                            super.onCharacteristicRead(gatt, characteristic, status);
+                            Log.d(TAG, "Char read");
+                        }
+
+                        @Override
+                        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+                            super.onCharacteristicChanged(gatt, characteristic);
+                            Log.d(TAG, "Char change");
+                        }
+                    });
+                } else {
+                    Log.d(TAG, "No devices");
+                }
             }
         };
-        long SCAN_PERIOD = 10000; //Time to scan in ms
+
+        long SCAN_PERIOD = 1000; //Time to scan in ms
         timer.schedule(task, SCAN_PERIOD);
 
         mScanning = true;
         mBLEAdapter.startLeScan(mBLECallback);
-        Log.d(TAG, "BLE Scan started");
+        Log.d(TAG, "BLE Scan Started");
     }
 }
