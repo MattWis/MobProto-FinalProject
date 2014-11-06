63619b7ad483cbb6c88f7b5c2f2a19d486c4128b
dontworryaboutit <michael.searing@students.olin.edu>
2014-11-06 18:57:35 -0500

    Commented code

--- app/src/main/java/com/example/mwismer/mobproto_finalproject/BLEFinderCallback.java
+++ app/src/main/java/com/example/mwismer/mobproto_finalproject/BLEFinderCallback.java
@@ -18,6 +18,7 @@ import java.util.concurrent.ArrayBlockingQueue;
 /**
  * Created by mwismer on 11/3/14.
  */
+ // Callback used to get us the results from the server (device)
 public class BLEFinderCallback extends BluetoothGattCallback {
     private ArrayList<UUID> mUUIDs = new ArrayList<UUID>();
     private ArrayBlockingQueue<Runnable> infoToGet = new ArrayBlockingQueue<Runnable>(128);
@@ -27,6 +28,7 @@ public class BLEFinderCallback extends BluetoothGattCallback {
     private HashMap<String, byte[]> characteristicMap = new HashMap<String, byte[]>();
     private HashMap<String, byte[]> descriptorMap = new HashMap<String, byte[]>();
 
+    // Client connection state has changed
     @Override
     public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
         super.onConnectionStateChange(gatt, status, newState);
@@ -43,39 +45,54 @@ public class BLEFinderCallback extends BluetoothGattCallback {
         success = successful;
     }
 
+    // Server's list of (remote services, characteristics and descriptors) for the remote device (server) has been updated
     @Override
     public void onServicesDiscovered(final BluetoothGatt gatt, int status) {
         super.onServicesDiscovered(gatt, status);
+        
+        // Check each service within device...
         for (BluetoothGattService service: gatt.getServices()) {
+            // List of characteristics in service
             List<BluetoothGattCharacteristic> characteristicList = service.getCharacteristics();
+            
+            // Check each characteristic within each service...
             for (final BluetoothGattCharacteristic characteristic: characteristicList) {
-                if (mUUIDs.indexOf(characteristic.getUuid()) == -1) {
+                if (mUUIDs.indexOf(characteristic.getUuid()) == -1) { // New characteristic! (not already in our list)
                     infoToGet.add(new Runnable() {
                         @Override
                         public void run() {
+                            // Request specific characteristic relating to newly discovered or updated data
                             updateSuccess(gatt.readCharacteristic(characteristic));
                         }
                     });
+                    // Add characteristic ID to our list
                     mUUIDs.add(characteristic.getUuid());
                 }
 
+                // List of descriptors in characteristic
                 List<BluetoothGattDescriptor> descriptorList = characteristic.getDescriptors();
+                // Check each descriptor within each characteristic...
                 for (final BluetoothGattDescriptor descriptor: descriptorList) {
-                    if (mUUIDs.indexOf(descriptor.getUuid()) == -1) {
+                    if (mUUIDs.indexOf(descriptor.getUuid()) == -1) { // New descriptor! (not already in our list)
                         infoToGet.add(new Runnable() {
                             @Override
                             public void run() {
+                                // Request specific descriptor relating to newly discovered or updated data
                                 updateSuccess(gatt.readDescriptor(descriptor));
                             }
                         });
+                        // Add descriptor ID to our list
                         mUUIDs.add(descriptor.getUuid());
                     }
                 }
             }
+            
+            // Execute read requests before moving on to next service
             readNextBLE(gatt);
         }
     }
 
+    // Returns response to characteristic read request
     @Override
     public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
         super.onCharacteristicRead(gatt, characteristic, status);
@@ -84,6 +101,7 @@ public class BLEFinderCallback extends BluetoothGattCallback {
         readNextBLE(gatt);
     }
 
+    // Returns response to descriptor read request
     @Override
     public void onDescriptorRead(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
         super.onDescriptorRead(gatt, descriptor, status);
@@ -92,6 +110,7 @@ public class BLEFinderCallback extends BluetoothGattCallback {
         readNextBLE(gatt);
     }
 
+    // Execute read requests stored in infoToGet, and log the result
     private void readNextBLE(BluetoothGatt gatt) {
         while (!infoToGet.isEmpty()) {
             infoToGet.poll().run();
@@ -102,6 +121,7 @@ public class BLEFinderCallback extends BluetoothGattCallback {
         }
     }
 
+    // Log all of the new information
     private void bulkLog() {
         Log.d(TAG, "Characteristics: ");
         for (String uuid: characteristicMap.keySet()) {
