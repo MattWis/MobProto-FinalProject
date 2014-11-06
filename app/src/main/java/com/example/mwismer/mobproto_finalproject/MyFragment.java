1d0736b8e8e8e1ec1dd2a886ba6675a5eba363e1
dontworryaboutit <michael.searing@students.olin.edu>
2014-11-06 18:02:16 -0500

    Commented code

--- app/src/main/java/com/example/mwismer/mobproto_finalproject/MyFragment.java
+++ app/src/main/java/com/example/mwismer/mobproto_finalproject/MyFragment.java
@@ -30,13 +30,16 @@ public class MyFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_my, container, false);
+        // To detect hardware BLE:
+        // if(getActivity().getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {}
         final BluetoothManager bluetoothManager =
                 (BluetoothManager) getActivity().getSystemService(Context.BLUETOOTH_SERVICE);
-        mBLEAdapter = bluetoothManager.getAdapter();
-        if (mBLEAdapter == null || !mBLEAdapter.isEnabled()) {
+        mBLEAdapter = bluetoothManager.getAdapter(); // Get device's "Bluetooth radio"
+        if (mBLEAdapter == null || !mBLEAdapter.isEnabled()) { // If Bluetooth doesn't exist or is disabled
             Log.d(TAG, "Enabling Bluetooth");
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            getActivity().startActivityForResult(enableBtIntent, 20);
+            getActivity().startActivityForResult(enableBtIntent, 20); // REQUEST_ENABLE_BT is 20
+            // REQUEST_ENABLE_BT will be the request code passed to onActivityResult callback in main activity
         }
 
         scanBLE();
@@ -61,21 +64,23 @@ public class MyFragment extends Fragment{
             @Override
             public void run() {
                 mScanning = false;
-                mBLEAdapter.stopLeScan(mBLECallback);
+                mBLEAdapter.stopLeScan(mBLECallback); // Stop scanning
                 Log.d(TAG, "BLE Scan finished");
-                if (device == null) {
+                if (device == null) { // Didn't find a device in alotted time
                     Log.d(TAG, "No devices");
                 } else {
+                    // Connect to GATT server hosted by found device (autoConnect is off)
+                    // Assign GATT callback handler to receive asynchronous callbacks
                     device.connectGatt(getActivity(), false, new BLEFinderCallback());
                 }
             }
         };
 
         long SCAN_PERIOD = 10000; //Time to scan in ms
-        timer.schedule(task, SCAN_PERIOD);
+        timer.schedule(task, SCAN_PERIOD); // Schedule scan to stop after SCAN_PERIOD
 
         mScanning = true;
-        mBLEAdapter.startLeScan(mBLECallback);
+        mBLEAdapter.startLeScan(mBLECallback); // Start scanning
         Log.d(TAG, "BLE Scan Started");
     }
 }
